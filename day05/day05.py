bdec191d02a52155d5cc21c7a4db859cf1c900ce
Sebastien Brisard <sebastien.brisard@m4x.org>
2018-12-06 06:11:54 +0100

    Operate on strings only

--- day05/day05.py
+++ day05/day05.py
@@ -6,11 +6,10 @@ def should_react(unit1, unit2):
 
 
 def react_full(polymer):
-    polymer = list(polymer)
     i = 0
     while i < len(polymer)-1:
         if should_react(polymer[i], polymer[i+1]):
-            del polymer[i:i+2]
+            polymer = polymer[:i]+polymer[i+2:]
             if i > 0:
                 # Step back in order to account for new reactions that
                 # might have been made possible by the present
@@ -18,7 +17,7 @@ def react_full(polymer):
                 i -= 1
         else:
             i += 1
-    return ''.join(polymer)
+    return polymer
 
 
 def test():
@@ -36,25 +35,21 @@ def main():
     with open('input.txt', 'r') as f:
         polymer = f.readline().strip()
 
-    #polymer1 = react_full(polymer)
     polymer1 = react_full(polymer)
-    print('After all possible reactions, the resulting polymer contain {} units'.format(len(polymer1)))
+    print('After all possible reactions, the resulting polymer contain {} '
+          'units'.format(len(polymer1)))
 
     minlen = len(polymer)
     for unit in string.ascii_lowercase:
-        print('Testing unit '+unit)
-        print('  Initial length: {}'.format(len(polymer)))
         polymer2 = polymer.replace(unit, '').replace(unit.upper(), '')
-        print('  After removing units of type {}: {}'.format(unit,
-                                                             len(polymer2)))
         polymer2 = react_full(polymer2)
-        print('  After fully reacting: {}'.format(len(polymer2)))
         if len(polymer2) < minlen:
             minlen = len(polymer2)
             worst_unit = unit
 
     print('The unit causing most problems is: '+worst_unit)
-    print('After removing this unit, the length of the fully reacted polymer is: {}'.format(minlen))
+    print('After removing this unit, the length of the fully reacted polymer '
+          'is: {}'.format(minlen))
 
 
 if __name__ == '__main__':
