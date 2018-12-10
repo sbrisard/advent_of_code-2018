1734a89a2fec9904058e3c5004bead57a1ca70b5
Sebastien Brisard <sebastien.brisard@m4x.org>
2018-12-10 06:31:20 +0100

    Accounting for multiples of 23

--- day09/day09.c
+++ day09/day09.c
@@ -4,6 +4,7 @@
 #include <glib.h>
 
 #define NUM_MARBLES 100
+#define NUM_PLAYERS 9
 
 typedef struct {
     int number;
@@ -14,6 +15,8 @@ typedef struct {
     GSList* circle;
     int length;
     int current;
+    int *scores;
+    int num_players;
 } MarbleGame;
 
 
@@ -22,11 +25,17 @@ MarbleGame* marble_game_new() {
     game->circle = NULL;
     game->length = 0;
     game->current = 0;
+    game->num_players = NUM_PLAYERS;
+    game->scores = g_new(gint, NUM_PLAYERS);
+    for (int player = 0; player < NUM_PLAYERS; player++) {
+	game->scores[player] = 0;
+    }
 }
 
 
 void marble_game_free(MarbleGame* game) {
     g_slist_free(game->circle);
+    g_free(game->scores);
     g_free(game);
 }
 
@@ -63,11 +72,26 @@ int main() {
 	marbles[i].number = i;
     }
     MarbleGame* game = marble_game_new();
+    int player = 0;
     for (int i = 0; i < 23; i++) {
-	marble_game_add(game, marbles+i); marble_game_print(game);
+	if (i % 23 != 0) {
+	    marble_game_add(game, marbles+i); marble_game_print(game);
+	} else {
+	    game->scores[player] += i;
+	    int position = game->current-7;
+	    if (position < 0) position += game->length;
+	    GSList* to_remove = g_slist_nth(game->circle, position);
+	    game->scores[player] += ((Marble*)to_remove->data)->number;
+	    game->circle = g_slist_remove(game->circle, to_remove);
+	    game->length -= 1;
+	    if (position > game->length) {position -= game->length;}
+	    game->current = position;
+	}
 	printf("\n");
+	player = (player+1) % NUM_PLAYERS;
     }
 
+    g_free(game->scores);
     marble_game_free(game);
     g_free(marbles);
 
